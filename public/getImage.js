1bbe46ddcd9db321e73fd255832496df9524aec3
Oskar Lindblad <lindbladoskar@gmail.com>
2020-04-26 15:55:02 +0200

    message can find words

--- public/getImage.js
+++ public/getImage.js
@@ -1,4 +1,22 @@
-function getImage(msg){
-    console.log(msg
-        )
+function checkForDashes(message){
+    requestsAPI = message.includes('/')
+    if(requestsAPI){
+        let wordCheck
+        wordCheck = message.split(" ")
+        for (let i = 0; i < wordCheck.length; i++) {
+            let ifcludesDash = wordCheck[i].charAt(0)
+            if(ifcludesDash == '/'){
+                wordCheck[i] = sendForAPI(wordCheck[i].substring(1))
+            }
+        }
+
+        message = wordCheck.join(' ')
+    }
+    
+    return message
+}
+
+function sendForAPI(word){
+    word = 'korv'
+    return word
 }
--- public/logic.js
+++ public/logic.js
@@ -139,8 +139,8 @@ function onSendMessage(event) {
     console.log('I am clicked!')
     
     const input = document.querySelector('.messageInput input')
-    getImage(input)
-    socket.emit('message', input.value)
+    const message = checkForDashes(input.value)
+    socket.emit('message', message)
     input.value = ""
 }
 
