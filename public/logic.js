d220d08ddf19a495fb69c7b47c6a78b0c92a2eca
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-04-28 11:31:45 +0200

    chat scrolls down if full and emptys on roomchange

--- public/logic.js
+++ public/logic.js
@@ -20,7 +20,7 @@ function init(){
     const messageFormInput = document.querySelector('.messageInput input')
     messageFormInput.addEventListener('input', detectWriting)
     const newRoomForm = document.querySelector('.newRoomForm button')
-    newRoomForm.addEventListener('click', newRoom)
+    newRoomForm.addEventListener('click', onGoToNewRoom)
 
     listAllRooms()
 }
@@ -100,6 +100,7 @@ socket.on('message', (message) => {
     listItem.innerHTML = message.message
 
     list.appendChild(listItem)
+    list.scrollTop = list.scrollHeight
 } )
 
 
@@ -164,6 +165,8 @@ function detectWriting() {
 } */
 
 function changeRoom(newRoomInfo){
+    let messageContainer = document.querySelector('.chatMessages')
+    messageContainer.innerHTML = ''
     // event.preventDefault()
     // const roomInputEl = document.querySelector('.changeRoomForm input')
     // const roomName = roomInputEl.value
@@ -182,7 +185,7 @@ setInterval(function(){
     document.querySelector('.someoneIsTyping').innerHTML = ""
  }, 2000);
 
-function newRoom(event){
+function onGoToNewRoom(event){
     event.preventDefault()
     const inputNewRoomEl = document.getElementById('newRoomNameIn')
     const inputNewRoomPassword = document.getElementById('newRoomPasswordIn')
